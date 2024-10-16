e12d50769a9ca3899e11784e5292f0a27ab9bc79
Suelen regina <suelen.regina.08@email.com>
2024-10-16 20:56:20 -0300

    atualizações

--- sw.js
+++ sw.js
@@ -20,3 +20,37 @@ self.addEventListener('fetch', (event) => {
         })
     );
 });
+
+
+const CACHE_NAME = 'cep-app-v2'; // Altere para v2, v3 conforme necessário
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                './index.html',
+                './style.css',
+                './app.js',
+                './manifest.json',
+                './icons/icon-192x192.png',
+                './icons/icon-512x512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    // Remove caches antigos
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
